c95b6e22b2ce3d6ca28eb4f727ae2ed79948355b
Pawel Cybulski <kontakt@pawelcybulski.pl>
2020-04-27 10:37:12 +0200

    [update] notice card style updated

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {ApiService} from './api.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,11 +8,37 @@ export class AuthService {
   private isAuthenticated = true;
   private role = 'volunteer'; // volunteer | needy
 
+  constructor(private apiService: ApiService) {
+  }
+
   isAuth(): boolean {
     return this.isAuthenticated;
   }
 
   getRole(): string {
     return this.role;
+  }
+
+  login(email: string, password: string) {
+    const data = {
+      email: email,
+      password: password
+    };
+
+    return this.apiService.post('users/login', data);
+  }
+
+  registration(name: string, email: string, phone: string, password: string, city: string, voivodeship: string, district: string) {
+    const data = {
+      name: name,
+      email: email,
+      phone: phone,
+      password: password,
+      city: city,
+      voivodeship: voivodeship,
+      district: district
+    };
+
+    return this.apiService.post('users', data);
   }
 }
