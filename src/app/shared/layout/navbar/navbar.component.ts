074b596007114736a50dbad2ff8c8f192617e68c
Paweł Cybulski <kontakt@pawelcybulski.pl>
2020-06-09 01:49:04 +0200

    navbar role update

--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -1,24 +1,36 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
   showNavbar = false;
   role: string;
   isAuth: boolean;
+
+  authSubscription: Subscription;
 
   constructor(private authService: AuthService) {
   }
 
   ngOnInit(): void {
-    this.role = this.authService.getRole();
-    this.isAuth = this.authService.isAuth();
+    this.authSubscription = this.authService.userAuthCredentialsSubject.subscribe(userCred => {
+      if (userCred && userCred.token) {
+        this.isAuth = true;
+        this.role = userCred.role;
+      } else {
+        this.isAuth = false;
+        this.role = null;
+      }
+    });
+  }
 
-    console.log(this.role, this.isAuth);
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 
   onToggleMenu() {
